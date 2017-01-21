f63b987824519fdd5b87079222b565a1d2530e1e
rxlabz <dev@rxlabz.com>
2017-01-21 23:29:26 +0100

    update and doc run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# kill port 8765 if used
+# TODO ask confirmation
 lsof -t -i tcp:8765 | xargs kill &
-MDSRV="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MDSRV="$( cd "$( dirname "${MDSRV}" )" && pwd )"
-echo '---'$MDSRV'----'
+
+# tools dir path
+MDSRV_TOOLS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# parent dir path
+MDSRV="$( cd "$( dirname "${MDSRV_TOOLS}" )" && pwd )"
+
+# empty styles.css before compiling sass
 > $MDSRV/web/styles.css &
+
+# compilation Sass vers styles.css
 dart-sass $MDSRV/web/styles.scss >> $MDSRV/web/styles.css &
+
+# launch md-server
 /bin/sleep 1 && dart /Users/rxlabz/dev/projects/darxlibz/mdsrv/bin/server.dart -d /Users/rxlabz/dev/notes/ &
+
+# open in browser
 /bin/sleep 2 && /usr/bin/open -a "/Applications/Google Chrome.app" "http://0.0.0.0:8765"
