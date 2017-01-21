abcd7a455cede24dc30996e27260f72e680aa3ea
rxlabz <dev@rxlabz.com>
2017-01-21 23:03:00 +0100

    update run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mdsrv="/Users/rxlabz/dev/projects/darxlibz/mdsrv/"
-
 lsof -t -i tcp:8765 | xargs kill &
-> $mdsrv/web/styles.css &
-dart-sass $mdsrv/web/styles.scss >> $mdsrv/web/styles.css  &
+MDSRV="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MDSRV="$( cd "$( dirname "${MDSRV}" )" && pwd )"
+echo '---'$MDSRV'----'
+> $MDSRV/web/styles.css &
+dart-sass $MDSRV/web/styles.scss >> $MDSRV/web/styles.css &
 /bin/sleep 1 && dart /Users/rxlabz/dev/projects/darxlibz/mdsrv/bin/server.dart -d /Users/rxlabz/dev/notes/ &
 /bin/sleep 2 && /usr/bin/open -a "/Applications/Google Chrome.app" "http://0.0.0.0:8765"
